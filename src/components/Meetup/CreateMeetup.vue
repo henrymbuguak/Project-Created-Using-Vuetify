0d47db0509708d83258043e2bb3fd8b2383fb88e
henrymbuguak <henrymbuguak@gmail.com>
2017-10-03 09:47:52 +0300

    Saving image into the firebase

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -24,9 +24,8 @@
           </v-layout>
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
-              <v-text-field name="imageUrl" v-model="imageUrl" label="Image Url" id="image-url" required>
-
-              </v-text-field>
+              <v-btn raised class="primary" @click="onPickFile">Upload Image</v-btn>
+              <input type="file" style="display: none" ref="fileInput" accept="image/*" @change="onFilePicked">
             </v-flex>
           </v-layout>
           <v-layout row>
@@ -78,7 +77,8 @@
         imageUrl: '',
         description: '',
         date: new Date(),
-        time: new Date()
+        time: new Date(),
+        image: null
       }
     },
     computed: {
@@ -108,15 +108,34 @@
         if (!this.formIsValid) {
           return
         }
+        if (!this.image) {
+          return
+        }
         const meetupData = {
           title: this.title,
           location: this.location,
-          imageUrl: this.imageUrl,
+          image: this.image,
           description: this.description,
           date: this.submittableDateTime
         }
         this.$store.dispatch('createMeetup', meetupData)
         this.$router.push('/meetups')
+      },
+      onPickFile () {
+        this.$refs.fileInput.click()
+      },
+      onFilePicked (event) {
+        const files = event.target.files
+        let filename = files[0].name
+        if (filename.lastIndexOf('.') <= 0) {
+          return alert('Please upload a valid image!')
+        }
+        const fileReader = new FileReader()
+        fileReader.addEventListener('load', () => {
+          this.imageUrl = fileReader.result
+        })
+        fileReader.readAsDataURL(files[0])
+        this.image = files[0]
       }
     }
   }
